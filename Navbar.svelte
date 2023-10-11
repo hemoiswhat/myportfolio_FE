53bd397d9dbdba7b6e3f3cea12651f155b50616e
hemoiswhat <heng.j.yap@gmail.com>
2023-10-11 15:16:23 +0800

    initial commit

---
+++ Navbar.svelte
@@ -0,0 +1,20 @@
+<script>
+    import {goto} from '$app/navigation';
+
+    
+</script>
+
+<main>
+    <p>
+        Hello, my name is John. <span>John is cool.</span> Be like John.
+      </p>
+      <form>
+        <label>Username</label>
+        <input type="text" />
+        <label>Password</label>
+        <input type="password" />
+        <input type="checkbox" />
+        <small>Stay logged in?</small>
+        <button on:click>Login</button>
+      </form>
+</main>
